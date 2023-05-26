2e70e26dddb2591a34052d0dd08f54a96453f801
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-05-26 18:46:30 +0300

    Add other block types present in the game

--- game.py
+++ game.py
@@ -15,26 +15,54 @@ class Game:
                 self._game[x][y] = self._map.get_block_type(x, y)
 
         self._block_empty = pygame.image.load(f"{resources_dir}/block_empty.png")
+        self._block_empty2 = pygame.image.load(f"{resources_dir}/block_empty2.png")
+        self._block_empty3 = pygame.image.load(f"{resources_dir}/block_empty3.png")
+        self._block_empty4 = pygame.image.load(f"{resources_dir}/block_empty4.png")
+        self._block_empty5 = pygame.image.load(f"{resources_dir}/block_empty5.png")
+        self._block_empty6 = pygame.image.load(f"{resources_dir}/block_empty6.png")
+        self._block_empty7 = pygame.image.load(f"{resources_dir}/block_empty7.png")
+        self._block_empty8 = pygame.image.load(f"{resources_dir}/block_empty8.png")
+        self._block_empty9 = pygame.image.load(f"{resources_dir}/block_empty9.png")
         self._block_brick = pygame.image.load(f"{resources_dir}/block_brick.png")
         self._concrete_empty = pygame.image.load(f"{resources_dir}/block_concrete.png")
         self._ladder_empty = pygame.image.load(f"{resources_dir}/block_ladder.png")
         self._block_water = pygame.image.load(f"{resources_dir}/block_water.png")
         self._block_thin_floor = pygame.image.load(f"{resources_dir}/block_thin_floor.png")
         self._block_treasure = pygame.image.load(f"{resources_dir}/block_treasure.png")
+        self._block_door_left = pygame.image.load(f"{resources_dir}/block_door_left.png")
+        self._block_door_right = pygame.image.load(f"{resources_dir}/block_door_right.png")
 
         self._block_types = [
             self._block_empty,      # 00
             self._concrete_empty,   # 01
             self._ladder_empty,     # 02
-            None,                   # 03
-            None,                   # 04
+            self._block_door_left,  # 03
+            self._block_door_right, # 04
             self._block_water,      # 05
             self._block_thin_floor, # 06
             self._block_treasure,   # 07
             self._block_treasure,   # 08            
-            None,                   # 09
-            None,                   # 0a
-            self._block_brick       # 0b
+            self._block_door_left,  # 09
+            self._block_door_right, # 0a
+            self._block_brick,      # 0b
+            None,                   # 0c
+            None,                   # 0d
+            self._block_empty2,     # 0e
+            None,                   # 0f
+            None,                   # 10
+            None,                   # 11
+            None,                   # 12
+            None,                   # 13
+            None,                   # 14
+            None,                   # 15
+            None,                   # 16
+            self._block_empty3,     # 17
+            self._block_empty4,     # 18
+            self._block_empty5,     # 19
+            self._block_empty6,     # 1a
+            self._block_empty7,     # 1b
+            self._block_empty8,     # 1c
+            self._block_empty9,     # 1d
         ]
 
 
