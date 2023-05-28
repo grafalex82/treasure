43b612653c89bc0a1408e6befbf98201a1901ed2
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-05-28 17:32:25 +0300

    Handle enemy falling

--- enemy.py
+++ enemy.py
@@ -8,14 +8,46 @@ class Enemy:
         self._pos = pos
         self._game = game
 
+        self._falling = False
+        self._tick = 0
+
         self._block_enemy = pygame.image.load(f"{resources_dir}/block_enemy.png")
 
 
+    def _on_surface(self):
+        if self._game.is_ladder(self._pos):
+            return True
+        
+        if self._game.is_thin_floor(self._pos.below()) and not self._falling:
+            return True
+
+        return not self._game.is_block_empty(self._pos.below())
+
+
+    def _try_move(self, new_pos):
+        if self._game.is_block_empty(new_pos) or \
+           self._game.is_ladder(new_pos):
+            self._pos = new_pos
+
+
+    def _handle_falling(self):
+        self._pos = self._pos.below()
+
+
     def _update_position(self):
-        pass
+        if self._on_surface():
+            self._falling = False
+        else:
+            self._falling = True
+            self._handle_falling()
+
 
     def update(self, screen):
-        self._update_position()
+        self._tick += 1                     
+
+        # Enemies move twice slower than the arrow
+        if self._tick % 2 == 0: 
+            self._update_position()
 
         screen.blit(self._block_enemy, (self._pos.x*CELL_WIDTH, self._pos.y*CELL_HEIGHT))
 
